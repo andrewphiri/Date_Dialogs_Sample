d55c145af4ee03979d8f2acbe854799f8d4c5e90
Andrew <andyphiri92@gmail.com>
2025-01-20 16:37:41 +0200

    Fixed landscape mode

--- app/src/main/java/com/example/datedialogssample/MainActivity.kt
+++ app/src/main/java/com/example/datedialogssample/MainActivity.kt
@@ -7,12 +7,22 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerFormatter
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,9 +37,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.example.datedialogssample.ui.theme.DateDialogsSampleTheme
 import java.time.format.DateTimeFormatter
 import java.util.Locale
@@ -63,17 +77,18 @@ fun ManufacturedDate() {
         val dateToString = millisToLocalDate?.let {
             DateUtils().dateToString(it)
         } ?: ""
-    Column {
-        DatePicker(
-            dateFormatter = DatePickerFormatter(
-                selectedDateSkeleton = "EE, dd MMM, yyyy",
-                ),
-            title = {
-                    Text(text = "Manufactured Date")
-            },
-            state = dateState,
-            showModeToggle = true
-        )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+                DatePicker(
+                    modifier = Modifier.verticalScroll(rememberScrollState()).scale(0.7f),
+                    dateFormatter = DatePickerFormatter(
+                        selectedDateSkeleton = "EE, dd MMM, yyyy",
+                    ),
+                    state = dateState,
+                    showModeToggle = true
+                )
+
         Text(
             modifier = Modifier
                 .fillMaxWidth()
@@ -93,6 +108,10 @@ fun DatePickerWithDialog(
     val millisToLocalDate = dateState.selectedDateMillis?.let {
         DateUtils().convertMillisToLocalDate(it)
     }
+
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+
     val dateToString = millisToLocalDate?.let {
         DateUtils().dateToString(millisToLocalDate)
     } ?: "Choose Date"
@@ -133,6 +152,9 @@ fun DatePickerWithDialog(
                 }
             ) {
                 DatePicker(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
                     state = dateState,
                     showModeToggle = true
                 )
